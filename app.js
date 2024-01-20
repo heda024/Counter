b3662d3541950ed2891c3ed1871df8502ebdc13f
heda024 <143176013+heda024@users.noreply.github.com>
2024-01-20 16:54:00 +0100

    update javascript

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ let counter = 0;
 const counterValue = document.getElementById('counter-value');
 const incrementBtn = document.getElementById('increment-btn');
 const decrementBtn = document.getElementById('decrement-btn');
-const resetBtn = document.querySelector('#reset');
  
 // To increment the value of counter
 incrementBtn.addEventListener('click', () => {
