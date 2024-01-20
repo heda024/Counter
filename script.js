a7b32fd9310f46a9a3e1eff62724822ffb7bf1b3
heda024 <143176013+heda024@users.noreply.github.com>
2024-01-20 17:00:01 +0100

    adjust JS

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 let counter = 0;
  
-const counterValue = document.getElementById('counter-value');
-const incrementBtn = document.getElementById('increment-btn');
-const decrementBtn = document.getElementById('decrement-btn');
+const counterValue = document.querySelector('.counter-value');
+const incrementBtn = document.querySelector('.increment');
+const decrementBtn = document.querySelector('.decrement');
  
 // To increment the value of counter
 incrementBtn.addEventListener('click', () => {
